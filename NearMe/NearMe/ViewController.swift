e8afea563b464bd6acf93615e16b44578b30ec94
fibeapp <motiw@me.com>
2023-01-10 17:34:28 +0600

    add mapview

--- NearMe/NearMe/ViewController.swift
+++ NearMe/NearMe/ViewController.swift
@@ -1,13 +1,32 @@
 import SwiftUI
+import MapKit
 
 class ViewController: UIViewController {
 
+    lazy var mapView: MKMapView = {
+        $0.showsUserLocation = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(MKMapView())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupUI()
+        setupConstraints()
     }
-
-
+    
+    private func setupUI() {
+        view.addSubview(mapView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
 struct ViewControllerRepresentable_Previews: PreviewProvider {
