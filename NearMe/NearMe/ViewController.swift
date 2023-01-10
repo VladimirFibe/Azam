1be5e19df0e2cbf2dfe433da8d85883c21a4766a
fibeapp <motiw@me.com>
2023-01-10 19:35:43 +0600

    add searchtextfield

--- NearMe/NearMe/ViewController.swift
+++ NearMe/NearMe/ViewController.swift
@@ -4,10 +4,22 @@ import MapKit
 class ViewController: UIViewController {
 
     lazy var mapView: MKMapView = {
-        $0.showsUserLocation = true
+//        $0.showsUserLocation = true
         $0.translatesAutoresizingMaskIntoConstraints = false
         return $0
     }(MKMapView())
+    
+    lazy var searchTextField: UITextField = {
+        $0.layer.cornerRadius = 10
+        $0.clipsToBounds = true
+        $0.backgroundColor = .white
+        $0.placeholder = "Search"
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        $0.leftViewMode = .always
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.returnKeyType = .go
+        return $0
+    }(UITextField())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +28,9 @@ class ViewController: UIViewController {
     }
     
     private func setupUI() {
+        view.addSubview(searchTextField)
         view.addSubview(mapView)
+        view.bringSubviewToFront(searchTextField)
     }
     
     private func setupConstraints() {
@@ -24,7 +38,12 @@ class ViewController: UIViewController {
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            searchTextField.heightAnchor.constraint(equalToConstant: 44),
+            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1),
+            searchTextField.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 1)
         ])
     }
 }
